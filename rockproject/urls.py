7f474aac1a1fe129836f3f8f945d241af103bf29
Will MacLean <maclean.will@gmail.com>
2024-03-19 14:32:33 -0500

    rock-view

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from rockapi.views import register_user, login_user
+from django.conf.urls import include
+from rest_framework import routers
+from rockapi.views import TypeView, RockView
 
 router = routers.DefaultRouter(trailing_slash=False)
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'rocks', RockView, 'rock')
+router.register(r'types', TypeView, 'type')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    
 ]
