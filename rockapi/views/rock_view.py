6d423d0831c4f3473881bd47efd3d99779df33a6
Will MacLean <maclean.will@gmail.com>
2024-03-20 10:55:54 -0500

    fixes

--- rockapi/views/rock_view.py
+++ rockapi/views/rock_view.py
@@ -2,7 +2,8 @@ from django.http import HttpResponseServerError
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
-from rockapi.models import Rock
+from rockapi.models import Rock, Type
+from django.contrib.auth.models import User
 
 
 class RockView(ViewSet):
@@ -32,10 +33,24 @@ class RockView(ViewSet):
         except Exception as ex:
             return HttpResponseServerError(ex)
 
+class RockOwnerSerializer(serializers.ModelSerializer):
 
-class RockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ( 'first_name', 'last_name' )
+
+class RockTypeSerializer(serializers.ModelSerializer):
     """JSON serializer"""
 
     class Meta:
+        model = Type
+        fields = ( 'label', )
+
+class RockSerializer(serializers.ModelSerializer):
+    """JSON serializer"""
+    type = RockTypeSerializer(many=False)
+    user = RockOwnerSerializer(many=False)
+
+    class Meta:
         model = Rock
-        fields = ( 'id', 'name', 'weight', )
+        fields = ( 'id', 'name', 'weight', 'type', 'user')
